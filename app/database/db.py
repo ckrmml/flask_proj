a8355b9b12b815a7742f6466ada248006d02e553
ckrmml <ckrummel@mail.de>
2020-08-25 15:49:44 +0200

    deploy-ready confs

--- app/database/db.py
+++ app/database/db.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///test.db', convert_unicode=True)
+
+engine = create_engine('mysql+pymysql://flask_proj:1234@localhost:3306/test', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     # >>> secrets.token_urlsafe()
     SECRET_KEY = 'Iw4nilrNMIYuhsSBQxnxhj0ntOPCjA3Ydmia4li4w4s'
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flask_proj:1234@localhost:3306/test'
 
     MAIL_SERVER = 'localhost'
     MAIL_PORT = 8025
@@ -23,11 +23,11 @@ class Config(object):
     ENABLE_LOGIN = True
 
     ENABLE_REGISTRATION = True
-    CONFIRM_REGISTRATION = True
-    MAIL_ON_CONFIRMATION = True
+    CONFIRM_REGISTRATION = False
+    MAIL_ON_CONFIRMATION = False
     LOGIN_ON_CONFIRMATION = True
 
-    ENABLE_PASSWORD_RESET = True
+    ENABLE_PASSWORD_RESET = False
 
     DEBUG_TB_ENABLED = True
     DEBUG_TB_HOSTS = 'http://127.0.0.1:5000'
